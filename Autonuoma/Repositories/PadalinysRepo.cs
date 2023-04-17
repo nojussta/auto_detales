a27bab1e21cb502fad201fb24e764eaec8c7cc1c
nojussta <nojus.stasiunas2@gmail.com>
2023-04-17 14:07:47 +0300

    add/repo-for-unit

---
+++ Autonuoma/Repositories/PadalinysRepo.cs
@@ -0,0 +1,28 @@
+﻿namespace Org.Ktu.Isk.P175B602.Autonuoma.Repositories;
+
+using MySql.Data.MySqlClient;
+using Org.Ktu.Isk.P175B602.Autonuoma.Models.Padaliniai;
+using Org.Ktu.Isk.P175B602.Autonuoma.Models.Paslauga;
+
+
+/// <summary>
+/// Database operations related to 'Paslauga' entity.
+/// </summary>
+public class PadalinysRepo
+{
+    public static List<Padalinys> List()
+    {
+        var query = $@"SELECT * FROM `padaliniai` ORDER BY id ASC";
+        var drc = Sql.Query(query);
+
+        var result =
+            Sql.MapAll<Padalinys>(drc, (dre, t) =>
+            {
+                t.Id = dre.From<int>("id");
+                t.Miestas = dre.From<string>("miestas");
+                t.Busena = dre.From<bool>("busena");
+            });
+
+        return result;
+    }
+}
