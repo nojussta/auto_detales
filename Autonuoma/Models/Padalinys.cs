0b00b3f746934a2cab1f88fdaa70f7a15c34c80a
nojussta <nojus.stasiunas2@gmail.com>
2023-04-17 14:07:37 +0300

    add/model-for-unit

---
+++ Autonuoma/Models/Padalinys.cs
@@ -0,0 +1,16 @@
+﻿namespace Org.Ktu.Isk.P175B602.Autonuoma.Models.Padaliniai;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+public class Padalinys
+{
+    [DisplayName("ID")]
+    public int Id { get; set; }
+
+    [DisplayName("Miestas")]
+    public string Miestas { get; set; }
+
+    [DisplayName("Būsena")]
+    public bool Busena { get; set; }
+}
