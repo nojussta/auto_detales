0df717bea468653b1256a6e9f065cf08acd755e0
nojussta <nojus.stasiunas2@gmail.com>
2023-04-17 14:10:38 +0300

    add/model-model-spec

---
+++ Autonuoma/Models/ModelioSpecifikacijos.cs
@@ -0,0 +1,32 @@
+﻿namespace Org.Ktu.Isk.P175B602.Autonuoma.Models.ModelioSpecifikacijos;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+public class ModelioSpecifikacijos
+{
+    [DisplayName("ID")]
+    public int ID { get; set; }
+
+    [DisplayName("Pavadinimas")]
+    [Required]
+    public string Pavadinimas { get; set; }
+
+
+    [DisplayName("Variklio turis")]
+    [Required]
+    public int VariklioTuris { get; set; }
+
+    [DisplayName("Galia")]
+    [Required]
+    public int Galia { get; set; }
+
+    [DisplayName("Kuro tipas")]
+    [Required]
+    public int KuroTipas { get; set; }
+
+    [DisplayName("Modelis")]
+    [Required]
+    public int FkModelis { get; set; }
+}
+
