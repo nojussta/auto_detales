95233c223df784274faf2796c3be1ffeb29a74e7
nojussta <nojus.stasiunas2@gmail.com>
2023-05-11 19:21:52 +0300

    add/contracts-model-change

--- autodetales/Models/ContractsReport.cs
+++ autodetales/Models/ContractsReport.cs
@@ -9,28 +9,57 @@ using System.ComponentModel.DataAnnotations;
 /// </summary>
 public class Sutartis
 {
-	[DisplayName("Sutartis")]
-	public int Nr { get; set; }
+    [DisplayName("Sutartis")]
+    public int Nr { get; set; }
 
-	[DisplayName("Data")]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-	public DateTime SutartiesData { get; set; }
+    [DisplayName("Data")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime SutartiesData { get; set; }
 
-	public string Vardas { get; set; }
+    public string Vardas { get; set; }
 
-	public string Pavarde { get; set; }
+    public string Pavarde { get; set; }
 
-	public string AsmensKodas { get; set; }
+    public string AsmensKodas { get; set; }
 
-	[DisplayName("Sudarytų sutarčių vertė")]
-	public decimal Kaina { get; set; }
+    [DisplayName("Sudarytų sutarčių vertė")]
+    public decimal Kaina { get; set; }
 
-	[DisplayName("Užsakytų paslaugų vertė")]
-	public decimal PaslauguKaina { get; set; }
+    [DisplayName("Užsakytų paslaugų vertė")]
+    public decimal PaslauguKaina { get; set; }
 
-	public decimal BendraSuma { get; set; }
+    public decimal BendraSuma { get; set; }
 
-	public decimal BendraSumaPaslaug { get; set; }
+    public decimal BendraSumaPaslaug { get; set; }
+
+    //new code
+    [DisplayName("Padalinys")]
+    public string Padalinys { get; set; }
+
+    [DisplayName("Data")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime Data { get; set; }
+
+    [DisplayName("Modelio pavadinimas")]
+    public string ModelioPavadinimas { get; set; }
+
+    [DisplayName("Dalies pavadinimas")]
+    public string Dalis { get; set; }
+
+    [DisplayName("Dalies sritis")]
+    public string DaliesSritis { get; set; }
+
+    [DisplayName("Variklio tūris")]
+    public int VariklioTuris { get; set; }
+
+    [DisplayName("Variklio galia")]
+    public int Galia { get; set; }
+
+    [DisplayName("Kuro tipas")]
+    public string KuroTipas { get; set; }
+
+    [DisplayName("Yra padaliniuose")]
+    public int PadaliniuSkaicius { get; set; }
 }
 
 /// <summary>
@@ -38,17 +67,27 @@ public class Sutartis
 /// </summary>
 public class Report
 {
-	[DataType(DataType.DateTime)]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-	public DateTime? DateFrom { get; set; }
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime? DateFrom { get; set; }
 
-	[DataType(DataType.DateTime)]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-	public DateTime? DateTo { get; set; }
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime? DateTo { get; set; }
 
-	public List<Sutartis> Sutartys { get; set; }
+    public List<Sutartis> Sutartys { get; set; }
 
-	public decimal VisoSumaSutartciu { get; set; }
+    public string VisoSumaSutartciu { get; set; }
 
-	public decimal VisoSumaPaslaugu { get; set; }
+    public int VisoSumaPaslaugu { get; set; }
+}
+
+public class Dalys
+{
+    public string Dalis { get; set; }
+    public string DaliesSritis { get; set; }
+    public int VariklioTuris { get; set; }
+    public int Galia { get; set; }
+    public string KuroTipas { get; set; }
+    public int DalysSkaicius { get; set; }
 }
---
+++ autodetales/Models/Skrydis.cs
@@ -0,0 +1,21 @@
+﻿namespace Org.Ktu.Isk.P175B602.Autonuoma.Models.Paslauga;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+/// <summary>
+/// Model of 'Paslauga' entity.
+/// </summary>
+public class Skrydis
+{
+	[DisplayName("Id")]
+	public int Id { get; set; }
+
+	[DisplayName("Data")]
+	[DataType(DataType.Date)]
+	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+	[Required]
+	public DateTime Data { get; set; }
+}
