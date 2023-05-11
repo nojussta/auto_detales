728f7e31ece58b3bf07fce895ce2ad312062f304
nojussta <nojus.stasiunas2@gmail.com>
2023-05-11 19:21:38 +0300

    add/reports-controller-update

--- autodetales/Controllers/ReportsController.cs
+++ autodetales/Controllers/ReportsController.cs
@@ -14,63 +14,63 @@ using ServicesReport = Org.Ktu.Isk.P175B602.Autonuoma.Models.ServicesReport;
 /// </summary>
 public class ReportsController : Controller
 {
-	/// <summary>
-	/// Produces services report.
-	/// </summary>
-	/// <param name="dateFrom">Starting date. Can be null.</param>
-	/// <param name="dateTo">Ending date. Can be null.</param>
-	/// <returns>Report view.</returns>
-	[HttpGet]
-	public ActionResult Services(DateTime? dateFrom, DateTime? dateTo)
-	{
-		var report = AtaskaitaRepo.GetTotalServicesOrdered(dateFrom, dateTo);
-		report.DateFrom = dateFrom;
-		report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
+    /// <summary>
+    /// Produces services report.
+    /// </summary>
+    /// <param name="dateFrom">Starting date. Can be null.</param>
+    /// <param name="dateTo">Ending date. Can be null.</param>
+    /// <returns>Report view.</returns>
+    [HttpGet]
+    public ActionResult Services(DateTime? dateFrom, DateTime? dateTo)
+    {
+        var report = AtaskaitaRepo.GetTotalServicesOrdered(dateFrom, dateTo);
+        report.DateFrom = dateFrom;
+        report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
 
-		report.Paslaugos = AtaskaitaRepo.GetServicesOrdered(dateFrom, dateTo);
+        report.Paslaugos = AtaskaitaRepo.GetServicesOrdered(dateFrom, dateTo);
 
-		return View(report);
-	}
+        return View(report);
+    }
 
-	/// <summary>
-	/// Produces contracts report.
-	/// </summary>
-	/// <param name="dateFrom">Starting date. Can be null.</param>
-	/// <param name="dateTo">Ending date. Can be null.</param>
-	/// <returns>Report view.</returns>
-	[HttpGet]
-	public ActionResult Contracts(DateTime? dateFrom, DateTime? dateTo)
-	{
-		var report = new ContractsReport.Report();
-		report.DateFrom = dateFrom;
-		report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
+    /// <summary>
+    /// Produces contracts report.
+    /// </summary>
+    /// <param name="dateFrom">Starting date. Can be null.</param>
+    /// <param name="dateTo">Ending date. Can be null.</param>
+    /// <returns>Report view.</returns>
+    [HttpGet]
+    public ActionResult Contracts(DateTime? dateFrom, DateTime? dateTo)
+    {
+        var report = new ContractsReport.Report();
+        report.DateFrom = dateFrom;
+        report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
 
-		report.Sutartys = AtaskaitaRepo.GetContracts(report.DateFrom, report.DateTo);
+        report.Sutartys = AtaskaitaRepo.GetDalys(report.DateFrom, report.DateTo);
 
-		foreach (var item in report.Sutartys)
-		{
-			report.VisoSumaSutartciu += item.Kaina;
-			report.VisoSumaPaslaugu += item.PaslauguKaina;
-		}
+        foreach (var item in report.Sutartys)
+        {
+            report.VisoSumaSutartciu = "Viso dalių: ";
+            report.VisoSumaPaslaugu += item.PadaliniuSkaicius;
+        }
 
-		return View(report);
-	}
+        return View(report);
+    }
 
-	/// <summary>
-	/// Produces late car returns reports.
-	/// </summary>
-	/// <param name="dateFrom">Starting date. Can be null.</param>
-	/// <param name="dateTo">Ending date. Can be null.</param>
-	/// <returns>Report view.</returns>
-	[HttpGet]
-	public ActionResult LateContracts(DateTime? dateFrom, DateTime? dateTo)
-	{
-		var report = new LateContractsReport.Report();
-		report.DateFrom = dateFrom;
-		report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
+    /// <summary>
+    /// Produces late car returns reports.
+    /// </summary>
+    /// <param name="dateFrom">Starting date. Can be null.</param>
+    /// <param name="dateTo">Ending date. Can be null.</param>
+    /// <returns>Report view.</returns>
+    [HttpGet]
+    public ActionResult LateContracts(DateTime? dateFrom, DateTime? dateTo)
+    {
+        var report = new LateContractsReport.Report();
+        report.DateFrom = dateFrom;
+        report.DateTo = dateTo?.AddHours(23).AddMinutes(59).AddSeconds(59); //move time of end date to end of day
 
-		report.Sutartys = AtaskaitaRepo.GetLateReturnContracts(report.DateFrom, report.DateTo);
+        report.Sutartys = AtaskaitaRepo.GetLateReturnContracts(report.DateFrom, report.DateTo);
 
-		return View(report);
-	}
+        return View(report);
+    }
 }
